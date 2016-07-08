a727c68a6bc04b7afb06c191af2a4902026bd6a0
Ben Delevingne <rockingskier@gmail.com>
2016-07-08 23:16:20 +0100

    sagasBuilder: Sagas builder utility

---
+++ src/sagasBuilder.js
@@ -0,0 +1,24 @@
+import { takeEvery } from 'redux-saga';
+import { call, put } from 'redux-saga/effects';
+
+
+export function sagasBuilder(request, REQUEST, actions) {
+  function* makeRequest({ type, payload }) {
+    try {
+      yield put(actions.pending());
+      const data = yield call(request, payload);
+      yield put(actions.success(data));
+    } catch (err) {
+      yield put(actions.failure(err));
+    }
+  }
+
+  function* watchForRequest() {
+    yield takeEvery(REQUEST, makeRequest);
+  }
+
+  return {
+    makeRequest,
+    watchForRequest,
+  };
+}
