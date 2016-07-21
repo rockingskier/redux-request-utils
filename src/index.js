743f4a5e8fa99d894e39627c523ee8d78090e0ca
Ben Delevingne <rockingskier@gmail.com>
2016-07-22 00:19:28 +0100

    index: INculde requestBuilder in export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as actionsBuilder } from './actionsBuilder.js';
 export { default as constantsBuilder } from './constantsBuilder.js';
+export { default as requestBuilder } from './requestBuilder.js';
 export { default as requestWrapper } from './requestWrapper.js';
 export { default as sagasBuilder } from './sagasBuilder.js';
