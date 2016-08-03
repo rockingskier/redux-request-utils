746578b5127fe9233f84a12c551dda2b10fd35b3
Ben Delevingne <rockingskier@gmail.com>
2016-08-04 00:01:22 +0100

    index: Import named methods

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-export { default as actionsBuilder } from './actionsBuilder.js';
-export { default as constantsBuilder } from './constantsBuilder.js';
-export { default as requestBuilder } from './requestBuilder.js';
-export { default as requestWrapper } from './requestWrapper.js';
-export { default as sagasBuilder } from './sagasBuilder.js';
+export { actionsBuilder } from './actionsBuilder.js';
+export { constantsBuilder } from './constantsBuilder.js';
+export { requestBuilder } from './requestBuilder.js';
+export { requestWrapper } from './requestWrapper.js';
+export { sagasBuilder } from './sagasBuilder.js';
